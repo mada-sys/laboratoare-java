c4e0dd74abfe5d9aa22b7bb57d4c44c67fe438fc
mada-sys <118816522+mada-sys@users.noreply.github.com>
2023-11-25 15:37:38 +0200

    Update Users.java

--- src/Users.java
+++ src/Users.java
@@ -1,4 +1,4 @@
-public class Users {
+public class Users implements Comparable<Users> {
     String country_name;
     String region;
     String income;
@@ -11,19 +11,20 @@ public class Users {
     Integer possible_cancer_cases;
     Integer possible_cancer_deaths;
 
-    public Users(String country_name,String region,String income,Integer year, Integer cohort_size,Integer coverage,Integer cancer_prevented,Integer deaths_prevented,String assumption_type, Integer possible_cancer_cases,Integer possible_cancer_deaths) {
+    public Users(String country_name, String region, String income, Integer year, Integer cohort_size, Integer coverage, Integer cancer_prevented, Integer deaths_prevented, String assumption_type, Integer possible_cancer_cases, Integer possible_cancer_deaths) {
         this.country_name = country_name;
         this.region = region;
         this.income = income;
-        this.year=year;
-        this.cohort_size=cohort_size;
-        this.coverage=coverage;
-        this.cancer_prevented=cancer_prevented;
-        this.deaths_prevented=deaths_prevented;
-        this.assumption_type=assumption_type;
-        this.possible_cancer_cases=possible_cancer_cases;
-        this.possible_cancer_deaths=possible_cancer_deaths;
+        this.year = year;
+        this.cohort_size = cohort_size;
+        this.coverage = coverage;
+        this.cancer_prevented = cancer_prevented;
+        this.deaths_prevented = deaths_prevented;
+        this.assumption_type = assumption_type;
+        this.possible_cancer_cases = possible_cancer_cases;
+        this.possible_cancer_deaths = possible_cancer_deaths;
     }
+
 
     public String getCountry_name() {
         return country_name;
@@ -38,7 +39,7 @@ public class Users {
     }
 
     public void setRegion(String region) {
-        this.region =region;
+        this.region = region;
     }
 
     public String getIncome() {
@@ -48,6 +49,7 @@ public class Users {
     public void setIncome(String income) {
         this.income = income;
     }
+
     public Integer getYear() {
         return year;
     }
@@ -55,6 +57,7 @@ public class Users {
     public void setYear(Integer year) {
         this.year = year;
     }
+
     public Integer getCohort_size() {
         return cohort_size;
     }
@@ -62,33 +65,37 @@ public class Users {
     public void setCohort_size(Integer cohort_size) {
         this.cohort_size = cohort_size;
     }
-    public Integer getCoverage(){
+
+    public Integer getCoverage() {
         return coverage;
     }
-    public void setCoverage(Integer coverage)
-    {
-        this.coverage=coverage;
+
+    public void setCoverage(Integer coverage) {
+        this.coverage = coverage;
     }
 
     public Integer getCancer_prevented() {
         return cancer_prevented;
     }
-    public void setCancer_prevented(Integer cancer_prevented)
-    {
-        this.cancer_prevented=cancer_prevented;
+
+    public void setCancer_prevented(Integer cancer_prevented) {
+        this.cancer_prevented = cancer_prevented;
     }
-    public Integer getDeaths_prevented(){
+
+    public Integer getDeaths_prevented() {
         return deaths_prevented;
     }
 
     public void setDeaths_prevented(Integer deaths_prevented) {
         this.deaths_prevented = deaths_prevented;
     }
-    public String getAssumption_type(){
+
+    public String getAssumption_type() {
         return assumption_type;
     }
-    public void setAssumption_type(String assumption_type){
-        this.assumption_type=assumption_type;
+
+    public void setAssumption_type(String assumption_type) {
+        this.assumption_type = assumption_type;
     }
 
     public Integer getPossible_cancer_cases() {
@@ -106,21 +113,9 @@ public class Users {
     public void setPossible_cancer_deaths(Integer possible_cancer_deaths) {
         this.possible_cancer_deaths = possible_cancer_deaths;
     }
-
     @Override
-    public String toString() {
-        return "Users{" +
-                "country_name='" + country_name + '\'' +
-                ", region=" + region +
-                ", income='" + income + '\'' +
-                ",year="+year + '\'' +
-                ",cohort_size="+ cohort_size+ '\''+
-                ",coverage=" + coverage + '\''+
-                ",cancer_prevented=" + cancer_prevented + '\''+
-                ",deaths_prevented=" + deaths_prevented + '\''+
-                ",assumption_type=" + assumption_type + '\''+
-                ", possible_cancer_cases=" + possible_cancer_cases + '\''+
-                ",possible_cancer_deaths=" + possible_cancer_deaths + '\''+
-                '}';
+    public int compareTo(Users o) {
+        return this.region.compareTo(o.region);
+
     }
 }

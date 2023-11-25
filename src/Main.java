d1b23f5ecf5f79edb2dbc739f3e8d56a74092fcf
mada-sys <118816522+mada-sys@users.noreply.github.com>
2023-11-25 15:38:48 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        IFileInputOutputService ioService= new FileInputOutputService();
+        IFileInputOutputService ioService = new FileInputOutputService();
         List<Users> usersFromFile = ioService.getUsersFromFile("src/hpv_2020s_results.csv");
-        for (var user: usersFromFile) {
-            System.out.println(user);
+
+        for (var users: usersFromFile) {
+            System.out.println(users);
         }
 
-        ioService.writeUsersToFile("src/hpv_2020s_results.csv",usersFromFile);
+        ioService.writeUsersToFile("src/hpv_2020s_results.csv", usersFromFile);
     }
 }
